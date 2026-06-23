ebb3e2ebd409cb0ec78f88224959119a81c00e7f
Richman | RO'Lyfe <richman@rootoflyfe.com>
2026-06-23 11:45:03 -0400

    Initialize line chart with market data

--- modules/charts.js
+++ modules/charts.js
@@ -1,1 +1,12 @@
+const ctx = document.getElementById("chart");
 
+const chart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["9:30", "10:00", "10:30", "11:00", "11:30", "12:00"],
+    datasets: [{
+      label: "Market Movement",
+      data: marketData.SPY
+    }]
+  }
+});
